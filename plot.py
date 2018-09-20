756bb54528755c0dde076fe2d4d7eb750e9cfa98
Chun Ly <astro.chun@gmail.com>
2018-09-19 22:31:37 -0700

    Use Notes column to select high elevation and tucson values; plot high el and tucson values

--- plot.py
+++ plot.py
@@ -48,6 +48,7 @@ def main(silent=False, verbose=True):
     data = read_excel(main_file)
 
     dates = data['Date'].values
+    comm0 = data['Notes'].values
     v1    = data['PF #1'].values
     v2    = data['PF #2'].values
     v3    = data['PF #3'].values
@@ -55,10 +56,18 @@ def main(silent=False, verbose=True):
     val = [v1, v2, v3]
     avg_pf = np.average(val, axis=0)
 
+    high_el = np.array([xx for xx in range(len(data)) if
+                        '8500' in str(comm0[xx])])
+    tucson  = np.array([xx for xx in range(len(data)) if
+                        'Tucson' in str(comm0[xx])])
+
     # data.Date = to_datetime(data['Date'], format='%Y-%m-%d %H:%M:%S.%f')
     # data.set_index(['Date'],inplace=True)
 
-    plt.scatter(dates, avg_pf, edgecolor='none', facecolor='b', alpha=0.5)
+    plt.scatter(dates[high_el], avg_pf[high_el], edgecolor='none',
+                facecolor='red', alpha=0.5)
+    plt.scatter(dates[tucson], avg_pf[tucson], edgecolor='none',
+                facecolor='blue', alpha=0.5)
     #data['Date'], data['PF #1'])
     #data.plot('Date', 'PF #1') #, xlim=[2016,2019])
     
